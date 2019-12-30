58f5e388da85c8adc7dc6ff6e88232db5b65051d
Jeannie <hello@jeannierombough.com>
2019-12-29 20:16:02 -0500

    Add react client side

---
+++ src/js/src/App.js
@@ -0,0 +1,157 @@
+import React, { Component } from "react";
+import Container from "./Container";
+import Footer from "./Footer";
+import "./App.css";
+import { getAllStudents } from "./client";
+import AddStudentForm from "./forms/AddStudentForm";
+import { errorNotification } from "./Notification";
+import { Table, Avatar, Spin, Icon, Modal, Empty } from "antd";
+
+const getIndicatorIcon = () => (
+  <Icon type="loading" style={{ fontSize: 24 }} spin />
+);
+
+class App extends Component {
+  state = {
+    students: [],
+    isFetching: false, //loading spin
+    isAddStudentModalVisible: false
+  };
+
+  componentDidMount() {
+    this.fetchStudents();
+  }
+
+  openAddStudentModal = () => this.setState({ isAddStudentModalVisible: true });
+  closeAddStudentModal = () =>
+    this.setState({ isAddStudentModalVisible: false });
+
+  fetchStudents = () => {
+    this.setState({
+      isFetching: true
+    });
+    getAllStudents()
+      .then(res =>
+        res.json().then(students => {
+          console.log(students);
+          this.setState({
+            students,
+            isFetching: false
+          });
+        })
+      )
+      .catch(error => {
+        console.log(error.error);
+        const message = error.error.message;
+        const description = error.error.error;
+        errorNotification(message, description);
+        this.setState({
+          isFetching: false
+        });
+      });
+  };
+
+  render() {
+    const { students, isFetching, isAddStudentModalVisible } = this.state;
+
+    const commonElements = () => (
+      <div>
+        <Modal
+          title="Add new student"
+          visible={isAddStudentModalVisible}
+          onOk={this.closeAddStudentModal}
+          onCancel={this.closeAddStudentModal}
+          width={1000}
+        >
+          <AddStudentForm
+            onSuccess={() => {
+              this.closeAddStudentModal();
+              this.fetchStudents();
+            }}
+            onFailure={(error) => {
+              const message = error.error.message;
+              const description = error.error.httpStatus;
+              errorNotification(message, description);
+            }}
+          />
+        </Modal>
+        <Footer
+          numberOfStudents={students.length}
+          handleAddStudentClickEvent={this.openAddStudentModal}
+        />
+      </div>
+    );
+
+    if (isFetching) {
+      return (
+        <Container>
+          <Spin indicator={getIndicatorIcon()} />
+        </Container>
+      );
+    }
+
+    // if we have students and something within it, display
+    if (students && students.length) {
+      // antd table - make sure table properties map JSON data that comes from our server
+      const columns = [
+        {
+          title: "",
+          key: "avatar",
+          render: (text, student) => (
+            <Avatar size="large">
+              {`${student.firstName
+                .charAt(0)
+                .toUpperCase()}${student.lastName.charAt(0).toUpperCase()}`}
+            </Avatar>
+          )
+        },
+        {
+          title: "Student Id",
+          dataIndex: "studentId",
+          key: "studentId"
+        },
+        {
+          title: "First Name",
+          dataIndex: "firstName",
+          key: "firstName"
+        },
+        {
+          title: "Last Name",
+          dataIndex: "lastName",
+          key: "lastName"
+        },
+        {
+          title: "Email",
+          dataIndex: "email",
+          key: "email"
+        },
+        {
+          title: "Gender",
+          dataIndex: "gender",
+          key: "gender"
+        }
+      ];
+
+      return (
+        <Container>
+          <Table
+            style={{ marginBottom: "100px" }}
+            dataSource={students}
+            columns={columns}
+            pagination={false}
+            rowKey="studentId"
+          />
+          {commonElements()}
+        </Container>
+      );
+    }
+
+    return (
+      <Container>
+        <Empty description={<h1>No students found</h1>} />
+        {commonElements()}
+      </Container>
+    );
+  }
+}
+export default App;
---
+++ src/js/src/forms/AddStudentForm.js
@@ -0,0 +1,124 @@
+import React from "react";
+import { Formik } from "formik";
+import { Input, Button, Tag } from "antd";
+import { addNewStudent } from "../client";
+
+const inputBottomMargin = { marginBottom: "10px" };
+const tagStyle = {
+  backgroundColor: "#f50",
+  color: "white",
+  ...inputBottomMargin
+};
+
+const AddStudentForm = (props) => (
+  <Formik
+    initialValues={{ firstName: "", lastName: "", email: "", gender: "" }}
+    validate={values => {
+      let errors = {};
+
+      if (!values.firstName) {
+        errors.firstName = "First name required";
+      }
+
+      if (!values.lastName) {
+        errors.lastName = "Last name required";
+      }
+
+      if (!values.email) {
+        errors.email = "Email required";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+      ) {
+        errors.email = "Invalid email address";
+      }
+
+      if (!values.gender) {
+        errors.gender = "Gender required";
+      } else if (!["MALE", "FEMALE"].includes(values.gender)) {
+        errors.gender = "Gender must be MALE or FEMALE";
+      }
+      return errors;
+    }}
+    onSubmit={(student, { setSubmitting }) => {
+      addNewStudent(student).then(() => {
+        props.onSuccess();
+      })
+      .catch(err => {
+          props.onFailure(err);
+      })
+      .finally(() => {
+        setSubmitting(false);
+      })
+    }}
+  >
+    {({
+      values,
+      errors,
+      touched,
+      handleChange,
+      handleBlur,
+      handleSubmit,
+      isSubmitting,
+      submitForm,
+      isValid
+      /* and other goodies */
+    }) => (
+      <form onSubmit={handleSubmit}>
+        <Input
+          style={inputBottomMargin}
+          name="firstName"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.firstName}
+          placeholder="First name"
+        />
+        {errors.firstName && touched.firstName && (
+          <Tag style={tagStyle}>{errors.firstName}</Tag>
+        )}
+        <Input
+          style={inputBottomMargin}
+          name="lastName"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.lastName}
+          placeholder="Last name"
+        />
+        {errors.lastName && touched.lastName && (
+          <Tag style={tagStyle}>{errors.lastName}</Tag>
+        )}
+        <Input
+          style={inputBottomMargin}
+          name="email"
+          type="email"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.email}
+          placeholder="Email"
+        />
+        {errors.email && touched.email && (
+          <Tag style={tagStyle}>{errors.email}</Tag>
+        )}
+        <Input
+          style={inputBottomMargin}
+          name="gender"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.gender}
+          placeholder="Gender"
+        />
+        {errors.gender && touched.gender && (
+          <Tag style={tagStyle}>{errors.gender}</Tag>
+        )}
+        <Button
+          onClick={() => submitForm()}
+          type="submit"
+          disabled={isSubmitting | (touched && !isValid)}
+        >
+          Submit
+        </Button>
+      </form>
+    )}
+  </Formik>
+);
+
+export default AddStudentForm;
